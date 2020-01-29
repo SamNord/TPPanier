b101ccfb81f9b796655f8c937935005a6e669827
Vanel <vntchadji@gmail.com>
2020-01-29 14:58:00 +0100

    MAJ class Basket

--- TP_Panier/Models/Basket.cs
+++ TP_Panier/Models/Basket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace TP_Panier.Models
 {
     class Basket
     {
+        private int id;
+        private decimal total;
+        private List<Product> produits;
+        public static SqlCommand command;
+
+        public int Id { get => id; set => id = value; }
+        public decimal Total { get => total; set => total = value; }
+        public List<Product> Produits { get => produits; set => produits = value; }
+
     }
 }
