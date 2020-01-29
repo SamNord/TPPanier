6a19a9e8d02d1da40c29b6c4d4484d8c53f1d943
SamNord <53309440+SamNord@users.noreply.github.com>
2020-01-29 16:59:58 +0100

    fenetre AddProduct et viewModel AddProductView en cours

---
+++ TP_Panier/ViewModels/AddProductView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_Panier.Models;
+
+namespace TP_Panier.ViewModels
+{
+    public class AddProductView 
+    {
+        private Product produit;
+
+        public AddProductView()
+        {
+            produit = new Product();
+        }
+    }
+}
---
+++ TP_Panier/Views/AddProduct.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TP_Panier.ViewModels;
+
+namespace TP_Panier.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Window
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+            DataContext = new AddProductView();
+        }
+    }
+}
