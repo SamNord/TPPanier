100aef55051c053ed0380a684932070b1dd3e886
Henri <henri.journel@gmail.com>
2020-01-29 14:57:55 +0100

    structure de base Customer.cs

--- TP_Panier/Models/Customer.cs
+++ TP_Panier/Models/Customer.cs
@@ -8,5 +8,19 @@ namespace TP_Panier.Models
 {
     public class Customer
     {
+        int id;
+        string firstName;
+        string lastName;
+        string phoneNumber;
+
+        public int Id { get => id; set => id = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+        public Customer()
+        {
+
+        }
     }
 }
