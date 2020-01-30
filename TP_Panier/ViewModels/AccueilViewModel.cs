418e69a72f06e960150ea5905de340ac99bc174b
SamNord <53309440+SamNord@users.noreply.github.com>
2020-01-30 14:49:16 +0100

    refaire branch dev en cours

---
+++ TP_Panier/ViewModels/AccueilViewModel.cs
@@ -0,0 +1,95 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_Panier.Models;
+
+namespace TP_Panier.ViewModels
+{
+    public class AccueilViewModel : ViewModelBase
+    {
+        private Customer customer;
+        private Product produit;
+        private Panier panier;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public AccueilViewModel()
+        {
+            customer = new Customer();
+            produit = new Product();
+            panier = new Panier();
+            ListProductPanier = new ObservableCollection<Product>();
+        }
+
+        public int IdClient { get => customer.Id; set => customer.Id = value; }
+        public string Lastname { get => customer.Lastname; set => customer.Lastname = value; }
+        public string Firstname { get => customer.Firstname; set => customer.Firstname = value; }
+        public string Phone { get => customer.Phone; set => customer.Phone = value; }
+        public Customer Customer { get => customer; set => customer = value; }
+
+        public int IdProduit { get => produit.Id; set => produit.Id = value; }
+        public string Label { get => produit.Label; set => produit.Label = value; }
+        public decimal Price { get => produit.Price; set => produit.Price = value; }
+        public Product Produit { get => produit; set { produit = value; RaisePropertyChanged(); } }
+
+        public Panier Panier { get => panier; set { panier = value; RaisePropertyChanged(); } }
+        public int IdPanier { get => panier.Id; set => panier.Id = value; }
+
+        public decimal Total
+        {
+            get => panier.Total;
+        }
+
+        public ObservableCollection<Product> ListProductPanier { get => listProductPanier; set { listProductPanier = value; RaisePropertyChanged(); } }
+
+        private ObservableCollection<Product> listProductPanier;
+
+        public void CalculTotal()
+        {
+            panier.Total = 0;
+            foreach (Product p in ListProductPanier)
+            {
+                panier.Total += p.Price;
+            }
+
+            RaisePropertyChanged("Total");
+        }
+
+        public bool ValiderPanier()
+        {
+            bool res = false;
+            Customer c = Customer.SearchCustomer(IdClient);
+            panier = new Panier()
+            {
+                Id = IdPanier,
+                //ClientId = IdClient,
+                ClientId = c.Id,
+                Total = Total
+            };
+            if (panier.Save())
+            {
+                res = true;
+                SaveInPanierProduit();
+            }
+
+            return res;
+        }
+
+        public void SaveInPanierProduit()
+        {
+            command = new SqlCommand("INSERT INTO PanierProduit (panier_id, produit_id) Values (@idPanier, @idProduit)", Configuration.connection);
+            command.Parameters.Add(new SqlParameter("@idPanier", IdPanier));
+            command.Parameters.Add(new SqlParameter("@idProduit", IdProduit));
+            Configuration.connection.Open();
+            command.ExecuteNonQuery();
+            command.Dispose();
+            Configuration.connection.Close();
+
+        }
+    }
+}
---
+++ TP_Panier/Views/AccueilView.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TP_Panier.Models;
+using TP_Panier.ViewModels;
+
+namespace TP_Panier.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour AccueilView.xaml
+    /// </summary>
+    public partial class AccueilView : Window
+    {
+        public static SqlCommand command;
+        public static SqlDataReader reader;
+
+        public AccueilView()
+        {
+            InitializeComponent();
+            DataContext = new AccueilViewModel();
+        }
+
+
+
+        private void AddClient(object sender, RoutedEventArgs e)
+        {
+            AccueilViewModel main = DataContext as AccueilViewModel;
+            if (main.Customer.Save())
+            {
+                MessageBox.Show("client ajouté avec le numéro " + main.Customer.Id);
+            }
+        }
+
+        private void SearchClient(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddInBasket(object sender, RoutedEventArgs e)
+        {
+            AccueilViewModel main = DataContext as AccueilViewModel;
+            main.Produit = Product.SearchProduct(main.Produit.Id);
+            main.ListProductPanier.Add(main.Produit);
+            main.CalculTotal();
+
+        }
+
+        private void ValiderPanier(object sender, RoutedEventArgs e)
+        {
+            AccueilViewModel main = DataContext as AccueilViewModel;
+
+            if (main.ValiderPanier())
+            {
+                MessageBox.Show("panier validé, le total est de " + main.Panier.Total + "€");
+            }
+
+            else
+            {
+                MessageBox.Show("erreur");
+            }
+
+
+        }
+
+        private void AddProductInBDD(object sender, RoutedEventArgs e)
+        {
+            AddProduct addProductWindow = new AddProduct();
+            addProductWindow.Show();
+        }
+
+        private void SearchProduct(object sender, RoutedEventArgs e)
+        {
+            AccueilViewModel main = DataContext as AccueilViewModel;
+            main.Produit = Product.SearchProduct(main.Produit.Id);
+            if (main.Produit != null)
+            {
+                MessageBox.Show("ok");
+            }
+            else
+            {
+                MessageBox.Show("produit non  trouvé");
+
+            }
+        }
+
+
+        /***********************Réinitialisation des tables*************
+         ***************************************************************/
+
+        private void DeleteClient(object sender, RoutedEventArgs e)
+        {
+            command = new SqlCommand("TRUNCATE Table client", Configuration.connection);
+            Configuration.connection.Open();
+            command.ExecuteNonQuery();
+            command.Dispose();
+            Configuration.connection.Close();
+        }
+
+        private void DeleteProduit(object sender, RoutedEventArgs e)
+        {
+            command = new SqlCommand("TRUNCATE Table produit", Configuration.connection);
+            Configuration.connection.Open();
+            command.ExecuteNonQuery();
+            command.Dispose();
+            Configuration.connection.Close();
+        }
+
+        private void DeletePanier(object sender, RoutedEventArgs e)
+        {
+            command = new SqlCommand("TRUNCATE Table panier", Configuration.connection);
+            Configuration.connection.Open();
+            command.ExecuteNonQuery();
+            command.Dispose();
+            Configuration.connection.Close();
+        }
+
+        private void DeleteProduitPanier(object sender, RoutedEventArgs e)
+        {
+            command = new SqlCommand("TRUNCATE Table panierproduit", Configuration.connection);
+            Configuration.connection.Open();
+            command.ExecuteNonQuery();
+            command.Dispose();
+            Configuration.connection.Close();
+        }
+
+        private void SearchOneClient(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
