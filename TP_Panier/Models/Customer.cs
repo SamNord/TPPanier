f297ba8b550a3f6f0a355c2c8f14463cda8003c4
Henri <henri.journel@gmail.com>
2020-01-30 11:12:28 +0100

    Finished customer class

--- TP_Panier/Models/Customer.cs
+++ TP_Panier/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace TP_Panier.Models
         string firstName;
         string lastName;
         string phoneNumber;
+        public static SqlCommand command;
+        private static SqlDataReader reader;
 
         public int Id { get => id; set => id = value; }
         public string FirstName { get => firstName; set => firstName = value; }
@@ -21,6 +24,58 @@ namespace TP_Panier.Models
         public Customer()
         {
 
+        }
+
+        public Customer(string firstName,string lastName,string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+        }
+
+
+        public bool Save()
+        {
+            bool result = false;
+            string request = "INSERT INTO contact(firstname, lastname, phonenumber) OUTPUT INSERTED.ID" +
+                " values (@firstname, @lastname, @phonenumber)";
+            command = new SqlCommand(request, Configuration.connection);
+            command.Parameters.Add(new SqlParameter("@lastname", FirstName));
+            command.Parameters.Add(new SqlParameter("@firstname", LastName));
+            command.Parameters.Add(new SqlParameter("@phonenumber", PhoneNumber));
+            Configuration.connection.Open();
+            Id = (int)command.ExecuteScalar();
+            if (Id > 0)
+            {
+                result = true;
+            }
+            command.Dispose();
+            Configuration.connection.Close();
+            return result;
+        }
+
+        public static Customer GetCustomerById(int id)
+        {
+            Customer customer = null;
+            string request = "SELECT TOP 1 id, firstname, lastname, phonenumber FROM customer where id = @id";
+            command = new SqlCommand(request, Configuration.connection);
+            command.Parameters.Add(new SqlParameter("@id", id));
+            Configuration.connection.Open();
+            reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                customer = new Customer()
+                {
+                    Id = reader.GetInt32(0),
+                    FirstName = reader.GetString(1),
+                    LastName = reader.GetString(2),
+                    PhoneNumber = reader.GetString(3),
+                };
+            }
+            reader.Dispose();
+            command.Dispose();
+            Configuration.connection.Close();
+            return customer;
         }
     }
 }
